e5faa424a38d50223d2c959893812ecf0e7ed744
SAMUEL OCRAN <samuelocran@SAMUELs-MacBook-Pro.local>
2019-07-06 18:50:26 +0100

    Integrate travis & coveralls

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # xyz
+[![Build Status](https://travis-ci.org/ocranbillions/xyz.svg?branch=develop)](https://travis-ci.org/ocranbillions/xyz)
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,11 @@
-'use strict';
+/* eslint-disable import/no-dynamic-require */
+import fs from 'fs';
+import { basename as _basename, join } from 'path';
+import Sequelize from 'sequelize';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const basename = _basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
@@ -17,15 +17,15 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+  .forEach((file) => {
+    const model = sequelize.import(join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -34,4 +34,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
