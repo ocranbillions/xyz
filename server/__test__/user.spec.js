0361ff85c961c62831c7691ce01d39bf24211e78
SAMUEL OCRAN <samuelocran@SAMUELs-MacBook-Pro.local>
2019-07-11 17:00:02 +0100

    signin test

---
+++ server/__test__/user.spec.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+import supertest from 'supertest';
+import app from '../app';
+
+const server = () => supertest(app);
+const url = '/api/v1';
+
+describe('Signup endpoint', () => {
+  it('Should register new user', (done) => {
+    server().post(`${url}/auth/signup`).send({
+      firstName: "sam",
+      lastName: "ocr",
+      email: "samsd@gmail.com",
+      password: 'password2',
+    })
+      .end((err, res) => {
+        expect(res.body.status).toEqual(201);
+        expect(res.body.data).toHaveProperty('token');
+        expect(res.body.data).toHaveProperty('id');
+        expect(res.body.data).toHaveProperty('firstName');
+        expect(res.body.data).toHaveProperty('lastName');
+        expect(res.body.data).toHaveProperty('email');
+        done();
+        expect(res.body).toMatchSnapshot();
+      });
+  });
+});
