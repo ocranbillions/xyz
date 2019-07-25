817ae411e99c86c704632615ce7f00c18387da75
SAMUEL OCRAN <samuelocran@SAMUELs-MacBook-Pro.local>
2019-07-25 09:58:55 +0100

    sre

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable indent */
 import { Router } from 'express';
 import UserController from '../controllers/userController';
 
@@ -9,14 +11,12 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
   clientID: '497050994431857',
   clientSecret: '37b58723d649b747c03a3384d177ef28',
-  callbackURL: 'http://localhost:3000/api/v1/auth/facebook/callback'
+  callbackURL: 'http://localhost:3000/api/v1/auth/facebook/callback',
+  profileFields: ['email'],
   },
-  function(accessToken, refreshToken, profile, done) {
-    console.log(accessToken, profile, refreshToken);
-    // User.findOrCreate(..., function(err, user) {
-    //     if (err) { return done(err); }
-    //     done(null, user);
-    //   });
+  function (accessToken, refreshToken, profile, done) {
+    // console.log(accessToken, profile, refreshToken);
+    return done(null, profile);
   }
 ));
 
@@ -29,7 +29,12 @@ router.get('/confirmation', UserController.confirmEmail);
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 router.get('/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/signin' }));
+  passport.authenticate('facebook', function(error, user, info) {
+    //   console.log(error, user, info);
+  }));
+
+// router.get('/auth/facebook/callback',
+// passport.authenticate('facebook', { successRedirect: '/',
+//                                     failureRedirect: '/login' }));
 
 export default router;
