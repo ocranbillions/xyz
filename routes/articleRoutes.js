86bca763f3d414390112654159a8c538b3f2473b
Samuel Ocran <sammiestt@gmail.com>
2019-07-31 17:58:47 +0100

    odun seeders

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import likeController from '../controllers/likesController';
+import AuthMiddlewware from '../middlewares/Authentication';
+
 
 const { likeArticle, dislikeArticle } = likeController;
+const { verifyToken } = AuthMiddlewware;
+
 
 const router = Router();
 
-router.post('/:id/like', likeArticle); // add middleware for auth
-router.post('/:id/dislike', dislikeArticle);
+router.post('/:id/like', verifyToken, likeArticle); // add middleware for auth
+router.post('/:id/dislike', verifyToken, dislikeArticle);
 
 export default router;
