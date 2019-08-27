9bc7a5c3010b51bf6e40656b81c24f8666603349
Emeka Ofe <40548599+mekzy-o@users.noreply.github.com>
2019-08-27 17:03:38 +0100

    ch(react router): set up react router (#6)
    Adds react router to the project 
    
    [Finishes #167165014]

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './app.scss';
+import Welcome from './components/views/Welcome';
+import NotFound from './components/views/NotFound';
 
 const App = () => (
   <Provider store={store}>
-    <div>
-      <h1>Hello Mazus</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   </Provider>
+
 );
 export default App;
---
+++ src/components/views/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h3>MAZUS TEAM is still building out this app, your page cannot be found at the moment</h3>
+  </div>
+);
+export default NotFound;
